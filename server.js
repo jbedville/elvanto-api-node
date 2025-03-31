acd484a2934082c182c84216eb7d15d561b73922
jbedville <jason@emergechurch.com.au>
2025-03-31 16:08:10 +1000

    added gitignore and env

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const service = require('./serviceArr.js')
 const app = express();
 app.use(cors())
 
+const getId = () => {
+    const response = client.apiCall("v1/services/getAll.json", {});
+            serviceArr = response.services.service.slice(0, 5).map((s) => s.id);
+}
+
 //API CALLS
     app.get("/api/service", async (req, res) => {
         try {
-            const response = client.apiCall("v1/services/getAll.json", {});
-            serviceArr = response.services.service.slice(0, 5).map((s) => s.id);
+            getId()
             res.json(serviceArr)
         } catch (error) {
             console.error("Error fetching services:", error);
@@ -22,7 +26,7 @@ app.use(cors())
     app.get("/location/name", async (req, res) => {
         try {
             const response = await client.apiCall("v1/services/getInfo.json", {id: "a8c0bdb9-cb14-4c14-a7f2-9a3855d1c226",});
-            locationName = response.service[0].location.name;
+            locationName = response.service[0]
             // console.log(locationName);
         res.json(locationName)
         } catch (error) {
