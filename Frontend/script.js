f06cacad32faebc3c6d68e1e708e5949f3052fbe
jbedville <jason@emergechurch.com.au>
2025-04-01 23:52:40 +1000

    SONGS WORKING RIGHT NOW

--- Frontend/script.js
+++ Frontend/script.js
@@ -9,17 +9,45 @@ async function fetchData() {
         ])
         console.log("Test Response: ", warner930)
 
-        const volunteers = warner930[0].volunteers.plan[0].positions.position.slice(2, 15) || [];
+        
 
-        volunteers.forEach(position => {
-            console.log(`${position.position_name}`)
-            position.volunteers?.volunteer?.forEach(v => {
-            console.log(`${v.person.firstname} ${v.person.lastname}`);
-        });
-});
+        //         volunteers.forEach(position => {
+        //             console.log(`${position.position_name}`)
+        //             position.volunteers?.volunteer?.forEach(v => {
+        //             console.log(`${v.person.firstname} ${v.person.lastname}`);
+        //         });
+        // });
+
+        const getVolunteers = (x) => {
+            const volunteers = x[0].volunteers.plan[0].positions.position.slice(2, 15) || [];
+        
+            volunteers.forEach(position => {
+                position.volunteers?.volunteer?.forEach(v => {
+                console.log(`${position.position_name}: ${v.person.firstname} ${v.person.lastname}`);
+                });
+            });
+        }
+        
+        console.log("<----MORAYFIELD 8:30---->")
+        getVolunteers(morayfield830)
+        console.log("")
+        console.log("<----WARNER 9:30---->")
+        getVolunteers(warner930)
+        console.log("")
+        console.log("<----REDCLIFFE 9:30---->")
+        getVolunteers(redcliffe930)
+        console.log("")
+        console.log("<----MORAYFIELD 10:30---->")
+        getVolunteers(morayfield1030)
+        console.log("")
+        console.log("<----WARNER 5:30---->")
+        getVolunteers(warner530)
+        
     } catch (error) {
         console.error("Oopsie Error:", error)
     }
 }
 
 fetchData()
+
+
