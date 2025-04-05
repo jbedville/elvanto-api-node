ff504b60d981ad2fca136c2bb4647ff081b08a34
jbedville <jason@emergechurch.com.au>
2025-04-05 11:24:41 +1000

    changed port

--- Backend/server.js
+++ Backend/server.js
@@ -139,7 +139,7 @@ app.get("/details/Warner530", async (req, res) => {
   }
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 5805;
 initServices().then(() => {
   console.log("Services Initialized")  
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
